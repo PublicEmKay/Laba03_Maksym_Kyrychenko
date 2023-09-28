3d8a29695770590525e51e87bd8e6724d83a2663
Maxim Kirichenko <public.emkay@gmail.com>
2023-09-28 17:27:46 +0300

    update pytest code

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,8 @@
-import calculator
+import app
 
 
 def test_addition():
-    assert calculator.add(2, 3) == 5
-    assert calculator.add(-1, 1) == 0
-    assert calculator.add(0, 0) == 0
-    assert calculator.add(-1, -1) == -2
+    assert app.add(2, 3) == 5
+    assert app.add(-1, 1) == 0
+    assert app.add(0, 0) == 0
+    assert app.add(-1, -1) == -2
