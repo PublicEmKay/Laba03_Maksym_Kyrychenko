3ad2b95bb257d02a7f10fd2c05f23046e39b7013
Maxim Kirichenko <public.emkay@gmail.com>
2023-09-28 17:00:49 +0300

    Added test

---
+++ test_calculator.py
@@ -0,0 +1,7 @@
+import calculator
+
+def test_addition():
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-1, 1) == 0
+    assert calculator.add(0, 0) == 0
+    assert calculator.add(-1, -1) == -2
