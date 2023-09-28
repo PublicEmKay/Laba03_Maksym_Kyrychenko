83f16d356874fc34c3731751ecbcb021fd8b518d
Maxim Kirichenko <public.emkay@gmail.com>
2023-09-28 17:21:04 +0300

    Edited formatting errors

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,8 @@
 import calculator
 
+
 def test_addition():
     assert calculator.add(2, 3) == 5
     assert calculator.add(-1, 1) == 0
     assert calculator.add(0, 0) == 0
     assert calculator.add(-1, -1) == -2
-
